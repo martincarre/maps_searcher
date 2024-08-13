537d6f834545588297ff2f50f31831cabd98226d
Martin Carre <m.carre86@gmail.com>
2024-08-13 11:45:27 +0200

    chmod the front end that was restricted for some reason...

---
+++ ms_app/src/app/ms/ms.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MapsRequestComponent } from './maps-request/maps-request.component';
+
+
+
+@NgModule({
+  declarations: [
+    MapsRequestComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MsModule { }
